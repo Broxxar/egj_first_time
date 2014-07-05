25ba7d355f72d42f45b6433e7ef17b266d698021
unknown <danjohnmoran@gmail.com>
2014-07-05 11:27:05 -0400

    Init, Input manager stuff

---
+++ Assets/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+	private static InputManager _instance;
+
+	public event MouseEventHandler GlobalDownAction = delegate {};
+	public event MouseEventHandler GlobalUpAction = delegate {};
+	
+	public static InputManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<InputManager>();
+			}
+			return _instance;
+		}
+
+		private set { }
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 worldPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collider = Physics2D.OverlapPoint(worldPosition);
+			GlobalDownAction(worldPosition);
+
+			if (collider != null)
+			{
+				Clickable clickable = collider.GetComponent<Clickable>();
+				if (clickable != null)
+				{
+					clickable.FireEvent(MouseEventType.Down, worldPosition);
+				}
+			}
+		}
+
+		else if (Input.GetMouseButtonUp(0))
+		{
+			Vector2 worldPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collider = Physics2D.OverlapPoint(worldPosition);
+			GlobalUpAction(worldPosition);
+			
+			if (collider != null)
+			{
+				Clickable clickable = collider.GetComponent<Clickable>();	
+				if (clickable != null)
+				{
+					clickable.FireEvent(MouseEventType.Up, worldPosition);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour
+{
+	bool _dragging;
+	LineHelper _lineHelper;
+
+	void Awake ()
+	{
+		GetComponent<Clickable>().DownAction += OnDownAction;
+		InputManager.Instance.GlobalUpAction += OnGlobalUpAction;
+	}
+
+	void OnGlobalUpAction (Vector3 position)
+	{
+	}
+
+	void OnDownAction (Vector3 position)
+	{
+	}
+
+	void Update ()
+	{
+	
+	}
+}
