0f96f9a9f2d5eea5b736ee88b584c3ef5b4076d3
Mallory Ketcheson <mallory.ketcheson@gmail.com>
2014-07-05 21:15:46 -0400

    Made the mechanics work again ..bout time!

--- Assets/LineManager.cs
+++ Assets/LineManager.cs
@@ -22,7 +22,7 @@ public class LineManager : MonoBehaviour {
 		private set { }
 	}
 	InputManager _inputManager;
-	List<Node> _pairs = new List<Node>();
+	List<Clocks> _pairs = new List<Clocks>();
 
 
 
@@ -35,50 +35,50 @@ public class LineManager : MonoBehaviour {
 	void OnGlobalDownAction (Vector3 position)
 	{
 
-		foreach(Node node in _pairs)
+		foreach(Clocks node in _pairs)
 		{
 			Line testLine = getNodesLine(node);
-			Vector3 spot = mouseIntersectedAt (position, testLine);
+			//Vector3 spot = mouseIntersectedAt (position, testLine);
 
 		}
 	}
-
-	Vector3 mouseIntersectedAt(Vector3 mousePos, Line line)
-	{	
-		Vector2 point1 = line.StartPos;
-		Vector2 point2 = line.EndPos;
-		float dx, dy, A, B, C, det, t;
-		float radius = .3f;
-
-		dx = point2.x - point1.x;
-		dy = point2.y - point1.y;
-
-		A = dx * dx + dy * dy;
-		B = 2 * (dx * (point1.x - mousePos.x) + dy * (point1.y - mousePos.y));
-		C = (point1.x - mousePos.x) * (point1.x - mousePos.x) + (point1.y - mousePos.y) * (point1.y - mousePos.y) - radius * radius;
-		
-		det = B * B - 4 * A * C;
-		if ((A <= 0.0000001) || (det < 0))
-		{
-			return Vector3.zero;
-		}
-		else if (det == 0)
-		{
-			// One solution.
-			t = -B / (2 * A);
-			return new Vector3(point1.x + t * dx, point1.y + t * dy, 0);
-
-		}
-		else
-		{
-			// Two solutions.
-			t = (float)((-B + Math.Sqrt(det)) / (2 * A));
-			return new Vector3(point1.x + t * dx, point1.y + t * dy,0);
-
-		}
-	}
+//
+//	Vector3 mouseIntersectedAt(Vector3 mousePos, Line line)
+//	{	
+//		Vector2 point1 = line.StartPos;
+//		Vector2 point2 = line.EndPos;
+//		float dx, dy, A, B, C, det, t;
+//		float radius = .3f;
+//
+//		dx = point2.x - point1.x;
+//		dy = point2.y - point1.y;
+//
+//		A = dx * dx + dy * dy;
+//		B = 2 * (dx * (point1.x - mousePos.x) + dy * (point1.y - mousePos.y));
+//		C = (point1.x - mousePos.x) * (point1.x - mousePos.x) + (point1.y - mousePos.y) * (point1.y - mousePos.y) - radius * radius;
+//		
+//		det = B * B - 4 * A * C;
+//		if ((A <= 0.0000001) || (det < 0))
+//		{
+//			return Vector3.zero;
+//		}
+//		else if (det == 0)
+//		{
+//			// One solution.
+//			t = -B / (2 * A);
+//			return new Vector3(point1.x + t * dx, point1.y + t * dy, 0);
+//
+//		}
+//		else
+//		{
+//			// Two solutions.
+//			t = (float)((-B + Math.Sqrt(det)) / (2 * A));
+//			return new Vector3(point1.x + t * dx, point1.y + t * dy,0);
+//
+//		}
+//	}
 	
-	public void AddPair(Node node)
+	public void AddPair(Clocks node)
 	{
 		if (!(_pairs.Contains (node) || _pairs.Contains (node.PartnerNode)))
 		{
@@ -87,7 +87,7 @@ public class LineManager : MonoBehaviour {
 	}
 	
 	
-	public void RemovePair(Node node)
+	public void RemovePair(Clocks node)
 	{
 		if(_pairs.Contains(node))
 		{
@@ -119,36 +119,37 @@ public class LineManager : MonoBehaviour {
 		return false; 
 	}
 
-	Line getNodesLine(Node node)
+	Line getNodesLine(Clocks node)
 	{
-		Vector2 startPos;
-		startPos.x = node.transform.position.x;
-		startPos.y = node.transform.position.y;
+		if (_pairs.Contains (node)) {
+						Vector2 startPos;
+						startPos.x = node.transform.position.x;
+						startPos.y = node.transform.position.y;
 
-		Vector2 endPos;
-		endPos.x = node.PartnerNode.transform.position.x;
-		endPos.y = node.PartnerNode.transform.position.y;
+						Vector2 endPos;
+						endPos.x = node.PartnerNode.transform.position.x;
+						endPos.y = node.PartnerNode.transform.position.y;
 
-		return new Line (startPos, endPos);
+						return new Line (startPos, endPos);
+				}
+		return null;
 	}
 
 	public bool ShouldLineBreak(Line mainLine)
 	{
-
 		bool shouldBreak = false; 
-		List<Node> remove = new List<Node> ();
-		foreach(Node node in _pairs){
+		List<Clocks> remove = new List<Clocks> ();
+		foreach(Clocks node in _pairs){
 			Line testLine = getNodesLine(node);
 			if(linesCrossed(mainLine, testLine)){
-				node.BreakLine();
+
 				shouldBreak = true;
 				remove.Add(node);
 
 			}
 		}
-		foreach(Node node in remove){
-			RemovePair(node);
-
+		if(remove.Count >1){
+			NodeManager.Instance.BreakAll();
 		}
 		return shouldBreak;
 	}
---
+++ Assets/NodeManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NodeManager : MonoBehaviour {
+	private static NodeManager _instance;
+
+	
+	public static NodeManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<NodeManager>();
+			}
+			return _instance;
+		}
+		
+		private set { }
+	}
+
+	List<Clocks> _clocks = new List<Clocks>();
+	public Vector2 MouseWorldPosition {get; private set;}
+	
+	public void AddNode(Clocks node)
+	{
+		if (!(_clocks.Contains (node) || _clocks.Contains (node.PartnerNode)))
+		{
+			_clocks.Add (node);
+		}
+	}
+	
+	
+	public void RemoveNode(Clocks node)
+	{
+		if(_clocks.Contains(node))
+		{
+			_clocks.Remove(node);
+		}else if(_clocks.Contains(node.PartnerNode))
+		{
+			_clocks.Remove (node.PartnerNode);
+		}	
+	}
+
+	public Clocks DifferentClockHit(Clocks current)
+	{
+		MouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Collider2D collider = Physics2D.OverlapPoint(MouseWorldPosition);
+		if (collider !=null)
+		if(Input.GetMouseButton(0))
+		{
+			foreach (Clocks node in _clocks) 
+			{	
+				if(node != current && collider.Equals(node.collider2D))
+				{
+					return node;
+				}
+			}
+		}
+		return null;
+	}
+
+	public void BreakAll(){
+		foreach (Clocks node in _clocks) 
+		{	
+
+				node.BreakLine();
+
+		}
+	}
+
+
+}
